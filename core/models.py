852161eb37d0619b361f6c539f9fc60e2d692bce
mumebee <mfayzillaeva@gmail.com>
2026-04-15 22:54:57 +0500

    class Place added to models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    TYPE_CHOICES = [
+        ('activity', 'Activity'),
+        ('restaurant', 'Restaurant'),
+        ('hotel', 'Hotel'),
+    ]
+
+    name = models.CharField(max_length = 200)
+    country = models.CharField(max_length = 100)
+    city = models.CharField(max_length = 100)
+
+    type = models.CharField(max_length = 20, choices = TYPE_CHOICES)
+
+    category = models.JSONField()
+    price_range = models.IntegerField()
+
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+    rating = models.FloatField(null=True, blank=True)
+
+    image = models.URLField() # URLs so no uploading
+
+    duration = models.IntegerField(null = True, blank = True)
+    cuisine_type = models.CharField(max_length = 100, null = True, blank = True)
+    hotel_stars = models.IntegerField(null = True, blank = True)
+
+    def __str__(self):
+        return self.name
