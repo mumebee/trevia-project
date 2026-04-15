924625a934aae1e987d1e4ab0dbd33d9987544a7
mumebee <mfayzillaeva@gmail.com>
2026-04-15 23:17:18 +0500

    abstract base classes and child class models added to models.py

--- core/models.py
+++ core/models.py
@@ -1,31 +1,67 @@
 from django.db import models
 
-class Place(models.Model):
-    TYPE_CHOICES = [
-        ('activity', 'Activity'),
-        ('restaurant', 'Restaurant'),
-        ('hotel', 'Hotel'),
-    ]
 
+# ABSTRACT BASE CLASS
+
+class BasePlace(models.Model):
     name = models.CharField(max_length = 200)
     country = models.CharField(max_length = 100)
     city = models.CharField(max_length = 100)
 
-    type = models.CharField(max_length = 20, choices = TYPE_CHOICES)
-
-    category = models.JSONField()
-    price_range = models.IntegerField()
-
     lat = models.FloatField()
     lng = models.FloatField()
 
-    rating = models.FloatField(null=True, blank=True)
+    # real price in USD
+    price = models.FloatField(help_text = "Price in USD")
 
-    image = models.URLField() # URLs so no uploading
+    rating = models.FloatField(null = True, blank = True)
+    image = models.URLField()
 
-    duration = models.IntegerField(null = True, blank = True)
-    cuisine_type = models.CharField(max_length = 100, null = True, blank = True)
-    hotel_stars = models.IntegerField(null = True, blank = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        abstract = True
+
+    # encapsulation with method to get location
+    def get_location(self):
+        return f"{self.city}, {self.country}"
+
+    # polymorphism to display card info, overriden in child classes
+    def display_card(self):
+        return f"{self.name} - ${self.price}"
 
     def __str__(self):
         return self.name
+
+"""
+CHILD CLASSES FROM ABSTRACT BASE CLASS BasePlace
+shows inheritance
+"""
+
+# ACTIVITY MODEL
+class Activity(BasePlace):
+    category = models.JSONField()  # ["museum", "nature"]
+    duration = models.IntegerField(help_text = "Duration in hours")
+
+    # polymorphism with adding category and duration
+    def display_card(self):
+        return f"{self.name}: - {self.duration}h - ${self.price}"
+
+
+# RESTAURANT MODEL
+class Restaurant(BasePlace):
+    cuisine_type = models.CharField(max_length=100)
+    is_cafe = models.BooleanField(default = False)
+
+    # polymorphism with adding cuisine type
+    def display_card(self):
+        return f"{self.name}: ({self.cuisine_type}) - ${self.price}"
+
+
+# HOTEL MODEL
+class Hotel(BasePlace):
+    stars = models.IntegerField(help_text = "1–5 stars")
+
+    # polymorphism with adding stars
+    def display_card(self):
+        return f"{self.name} ⭐{self.stars} - ${self.price}/night"
