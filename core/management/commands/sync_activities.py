93c4c5bc5d37fe14cc41e998273121dc469837c7
miisa23 <shuhratqizimunisaxon@gmail.com>
2026-05-03 19:43:35 +0500

    Munisa reg+log styles

---
+++ core/management/commands/sync_activities.py
@@ -0,0 +1,24 @@
+import json
+from pathlib import Path
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from core.models import Activity
+
+
+class Command(BaseCommand):
+    help = 'Sync activities images from JSON file'
+
+    def handle(self, *args, **kwargs):
+        json_path = settings.BASE_DIR / 'static' / 'data' / 'activities.json'
+        
+        self.stdout.write(f'Looking for: {json_path}')
+
+        with open(json_path, encoding='utf-8') as f:
+            data = json.load(f)
+
+        updated = 0
+        for item in data:
+            rows = Activity.objects.filter(id=item['id']).update(image=item['image'])
+            updated += rows
+
+        self.stdout.write(self.style.SUCCESS(f'Updated {updated} activities'))
